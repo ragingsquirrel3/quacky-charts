62371f93a63623aa6ca29fc4e78b6bbb60b9858d
Travis Sheppard <travissheppard.1988@gmail.com>
2013-01-27 19:12:03 -0500

    generator generates whats needed for line graphs

--- lib/generators/quacky/install_generator.rb
+++ lib/generators/quacky/install_generator.rb
@@ -7,15 +7,15 @@ module Quacky
       source_root File.expand_path("../templates", __FILE__)
       desc "This generator installs JS files needed for D3 pie charts"
       
-      def write_js_files
+      def write_helper_files
         # coffee builders
         copy_file 'pie_chart_builder.js.coffee', 'app/assets/javascripts/pie_chart_builder.js.coffee'
         copy_file 'line_graph_builder.js.coffee', 'app/assets/javascripts/line_graph_builder.js.coffee'
         
-        # vendor js libraries
-        vendor_dir = 'vendor/assets/javascripts'
-        copy_file 'd3.v2.min.js', "#{vendor_dir}/d3.v2.min.js"
-        copy_file 'rickshaw.min.js', "#{vendor_dir}/rickshaw.min.js"
+        # vendor files
+        copy_file 'd3.v2.min.js', 'app/assets/javascripts/d3.v2.min.js'
+        copy_file 'rickshaw.min.js', 'app/assets/javascripts/rickshaw.min.js'
+        copy_file 'rickshaw.min.css', 'app/assets/stylesheets/rickshaw.min.css'
       end
   
     end
